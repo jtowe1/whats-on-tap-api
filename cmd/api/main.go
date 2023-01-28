35e1043be7d54fdf029198e7b5eacdb9bdca87c5
Jeremiah <jtowe1@asu.edu>
2023-01-28 12:44:37 -0500

    Remove extra db params

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -17,10 +17,7 @@ type config struct {
 	port int
 	env  string
 	db   struct {
-		dsn          string
-		maxOpenConns int
-		maxIdleConns int
-		maxIdleTime  string
+		dsn string
 	}
 }
 
@@ -36,10 +33,6 @@ func main() {
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production")
 
 	flag.StringVar(&cfg.db.dsn, "db-dsn", "", "MySQL DSN")
-
-	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "MySQL max open connections")
-	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "MySQL max idle connections")
-	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "MySQL max connection idle time")
 
 	flag.Parse()
 
@@ -72,17 +65,6 @@ func openDB(cfg config) (*sql.DB, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	db.SetMaxOpenConns(cfg.db.maxOpenConns)
-
-	db.SetMaxIdleConns(cfg.db.maxIdleConns)
-
-	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
-	if err != nil {
-		return nil, err
-	}
-
-	db.SetConnMaxIdleTime(duration)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
